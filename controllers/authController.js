c6dae269b2a53217639a5235d7b4b5513da336d1
Carlos Gil <carlos.gil@chalmers.se>
2023-02-28 18:26:56 +0100

    changed env variable expires_in for JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,14 +15,13 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-
+  
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 3600 * 1000
     ),
     httpOnly: true,
   };
-
   // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;  // I think this requires https
 
   res.cookie('jwt', token, cookieOptions);
